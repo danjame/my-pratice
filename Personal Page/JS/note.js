38c445a528d76b78f35b016e15a402dec8e04b05
Danjame <40506250+Danjame@users.noreply.github.com>
2019-06-23 17:34:01 +0100

    myPage final version

--- Personal Page/JS/note.js
+++ Personal Page/JS/note.js
@@ -2,54 +2,48 @@
 const titleUp = getEle(".titleBox>ul>li:first-child");
 const titleMid = getEle(".titleBox>ul>li:nth-child(2)");
 const titleDown = getEle(".titleBox>ul>li:last-child");
-
+const artiSelection = getEle("#artiSelection");
 //箭头
 const arrowUp = getEle(".titleBox>ul>li:first-child div:last-child");
 const arrowDown = getEle(".titleBox>ul>li:last-child div:last-child");
-//浏览器高度
-let clientHeight = document.documentElement.clientHeight;
-//浮动导航适应手机
+//浮动导航适应手机和悬浮导航交互适应桌面端
 if (window.matchMedia("(max-device-width:425px)").matches) {
     autoTitleBox();
+}else{
+    arrowColorDesktop();
 }
 //浮动导航定位
 function autoTitleBox() {
-	const artiSelection = getEle("#artiSelection");
-	const titleBox = getEle(".titleBox");
-	const arrowDownBox = getEle(".titleBox>ul>li:last-child");
+    let clientHeight = document.documentElement.clientHeight;
+    const titleBox = getEle(".titleBox");
+    const arrowDownBox = getEle(".titleBox>ul>li:last-child");
     titleBox.style.top = `${clientHeight-titleBox.offsetHeight}px`;
     artiSelection.style.top = `${clientHeight-artiSelection.offsetHeight-arrowDownBox.offsetHeight}px`;
     artiSelection.style.right = `${titleBox.offsetWidth}px`;
 };
 
-titleUp.addEventListener("mouseover", function() {
-    arrowUp.style.borderBottomColor = "#eeeeee";
-}, false);
+function arrowColorDesktop() {
+    titleUp.addEventListener("mouseover", ()=> {
+        arrowUp.style.borderBottomColor = "#eeeeee";
+    }, false);
 
-titleDown.addEventListener("mouseover", function() {
-    arrowDown.style.borderTopColor = "#eeeeee";
-}, false);
+    titleDown.addEventListener("mouseover", ()=> {
+        arrowDown.style.borderTopColor = "#eeeeee";
+    }, false);
 
-titleUp.addEventListener("mouseout", function() {
-    arrowUp.style.borderBottomColor = "white";
-}, false);
+    titleUp.addEventListener("mouseout", ()=> {
+        arrowUp.style.borderBottomColor = "white";
+    }, false);
 
-titleDown.addEventListener("mouseout", function() {
-    arrowDown.style.borderTopColor = "white";
-}, false);
+    titleDown.addEventListener("mouseout", ()=> {
+        arrowDown.style.borderTopColor = "white";
+    }, false);
+}
 
-titleMid.addEventListener("mouseover", function() {
-    artiSelection.style.visibility = "visible";
-}, false);
-
-titleMid.addEventListener("mouseout", function() {
-    artiSelection.style.visibility = "hidden";
-}, false);
-
-artiSelection.addEventListener("mouseover", function() {
-    artiSelection.style.visibility = "visible";
-}, false);
-
-artiSelection.addEventListener("mouseout", function() {
-    artiSelection.style.visibility = "hidden";
+titleMid.addEventListener("click", function() {
+    if (artiSelection.style.visibility === "visible") {
+        artiSelection.style.visibility = "hidden";
+    } else {
+        artiSelection.style.visibility = "visible";
+    }
 }, false);
