16cf753d2afed69d69f083ea24d59c1e15fa702b
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2019-11-14 23:56:41 -0600

    building computeComponent

--- Account_Udemy_Project/app.js
+++ Account_Udemy_Project/app.js
@@ -4,7 +4,9 @@ let UIComponent = (() => {
         type: ".add__type",
         desc: ".add__description",
         amount: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeWrap: ".income__list",
+        expenseWrap: ".expenses__list",
     }
 
     return {
@@ -15,13 +17,44 @@ let UIComponent = (() => {
                 amount: document.querySelector(allDoms.amount).value
             }
         },
-
         getAllDoms() {
             return allDoms;
-        }
+        },
+        addToList(type, newItem) {
+            let parentWrap, itemHtml
+
+            if (type === "exp") {
+                parentWrap = document.querySelector(expenseWrap);
+                itemHtml = `<div class="item clearfix" id="expense-${newItem.id}">
+                                <div class="item__description">${newItem.desc}</div>
+                                <div class="right clearfix">
+                                    <div class="item__value">- ${newItem.value}</div>
+                                    <div class="item__percentage">21%</div>
+                                    <div class="item__delete">
+                                        <button class="item__delete--btn">
+                                        <i class="ion-ios-close-outline"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>`
+
+            } else if (type === "inc") {
+                parentWrap = document.querySelector(incomeWrap);
+                itemHtml = `<div class="item clearfix" id="income-${newItem.id}">
+                                <div class="item__description">${newItem.desc}</div>
+                                <div class="right clearfix">
+                                    <div class="item__value">+ ${newItem.value}</div>
+                                    <div class="item__delete">
+                                        <button class="item__delete--btn">
+                                        <i class="ion-ios-close-outline"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>`
+            }
+            parentWrap.insertAdjacentHTML("beforeend", itemHtml);
+        },
     }
-
-
 
 })();
 
@@ -57,7 +90,7 @@ let ComputeComponent = (() => {
             let newItem, id;
 
             if (data.allItems[type].length !== 0) {
-                data.allItems[type][data.allItems[type].length - 1].id + 1;
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 id = 0;
             }
@@ -67,10 +100,22 @@ let ComputeComponent = (() => {
             } else if (type === "inc") {
                 newItem = new Income(id, desc, value);
             }
-
             data.allItems[type].push(newItem);
             return newItem;
-        }
+        },
+        calculateTotal(type) {
+            let sum = 0;
+            if (type === "exp") {
+                data.allItems[type].forEach((item) => {
+                    sum += parseInt(item.value);
+                })
+            } else if (type === "inc") {
+                data.allItems[type].forEach((item) => {
+                    sum += parseInt(item.value);
+                })
+            }
+            data.totalAmount[type] = sum;
+        },
     }
 
 })()
@@ -85,8 +130,10 @@ let linkage = ((UIComponent, ComputeComponent) => {
         document.querySelector(doms.addBtn).addEventListener("click", () => {
             let inputValues = UIComponent.getInput();
             let addNewItem = ComputeComponent.addItem(inputValues.type, inputValues.desc, inputValues.amount);
-            console.log(inputValues.type);
-            console.log(addNewItem);
+            
+            ComputeComponent.calculateTotal(inputValues.type);
+            UIComponent.addToList(addNewItem);
+
         }, false)
     }
 
