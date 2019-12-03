bd1c97a3c01adfeb49f9ba4e3500d77c965b28d2
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2019-12-03 15:09:59 +0000

    function construtor code optimized

--- GameCharacter_Canvas/GameCharacter.js
+++ GameCharacter_Canvas/GameCharacter.js
@@ -3,35 +3,28 @@ class Character {
         this.ctx = document.querySelector(hero.canvasNode).getContext("2d");
         this.image = new Image();
         this.image.src = hero.imageSrc;
-
         this.imageCol = hero.imageCol;
         this.imageRow = hero.imageRow;
-
-
-        this.timer = null;
         this.index = hero.index;
 
-        this.wardSpeed = 100;
-        this.standSpeed = 300;
+        this.wardSpeed = hero.wardSpeed;
+        this.standSpeed = hero.standSpeed;
 
-        this.downCol = 0;
-        this.rightCol = 1;
-        this.leftCol = 2;
-        this.upCol = 3;
+        this.standStart = hero.standStart;
+        this.wardStart = hero.wardStart;
+        this.wardEnd = hero.wardEnd;
 
-        this.standStart = 0;
-        this.wardStart = 4;
-        this.wardEnd = 12;
+        this.timer = null;
     }
 
-    downWard() {
+    forWard(actionRow) {
         this.index = this.wardStart;
         const eachWidth = this.image.width / this.imageCol;
         const eachHeight = this.image.height / this.imageRow;
         const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
         const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
 
-        const downWard = () => {
+        const forWard = () => {
             clearTimeout(this.timer);
             this.timer = setTimeout(() => {
                 this.index++;
@@ -45,121 +38,25 @@ class Character {
                 );
                 this.ctx.drawImage(
                     this.image,
-                    this.index * eachWidth, this.downCol * eachHeight,
+                    eachWidth * this.index, eachHeight * actionRow,
                     eachHeight, eachHeight,
                     canCenterX, canCenterY,
                     eachHeight, eachHeight
                 );
-                downWard();
+                forWard();
             }, this.wardSpeed)
         };
-        downWard();
+        forWard()
     }
 
-    rightWard() {
-        this.index = this.wardStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const rightWard = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardEnd) {
-                    this.index = this.wardStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.rightCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                rightWard();
-            }, this.wardSpeed)
-        };
-        rightWard();
-    }
-
-    leftWard() {
-        this.index = this.wardStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const leftWard = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardEnd) {
-                    this.index = this.wardStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.leftCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                leftWard();
-            }, this.wardSpeed)
-        };
-        leftWard();
-    }
-
-    upWard() {
-        this.index = this.wardStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const upWard = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardEnd) {
-                    this.index = this.wardStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.upCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                upWard();
-            }, this.wardSpeed)
-        };
-        upWard();
-    }
-
-    downWardStand() {
+    stand(actionRow) {
         this.index = this.standStart;
         const eachWidth = this.image.width / this.imageCol;
         const eachHeight = this.image.height / this.imageRow;
         const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
         const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
 
-        const downWardStand = () => {
+        const stand = () => {
             clearTimeout(this.timer);
             this.timer = setTimeout(() => {
                 this.index++;
@@ -173,111 +70,15 @@ class Character {
                 );
                 this.ctx.drawImage(
                     this.image,
-                    this.index * eachWidth, this.downCol * eachHeight,
+                    eachWidth * this.index, eachHeight * actionRow,
                     eachHeight, eachHeight,
                     canCenterX, canCenterY,
                     eachHeight, eachHeight
                 );
-                downWardStand();
+                stand();
             }, this.standSpeed)
         };
-        downWardStand();
-    }
-
-    rightWardStand() {
-        this.index = this.standStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const rightWardStand = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardStart) {
-                    this.index = this.standStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.rightCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                rightWardStand();
-            }, this.standSpeed)
-        };
-        rightWardStand();
-    }
-
-    leftWardStand() {
-        this.index = this.standStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const leftWardStand = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardStart) {
-                    this.index = this.standStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.leftCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                leftWardStand();
-            }, this.standSpeed)
-        };
-        leftWardStand();
-    }
-
-    upWardStand() {
-        this.index = this.standStart;
-        const eachWidth = this.image.width / this.imageCol;
-        const eachHeight = this.image.height / this.imageRow;
-        const canCenterX = this.ctx.canvas.width / 2 - eachWidth / 2;
-        const canCenterY = this.ctx.canvas.height / 2 - eachHeight / 2;
-
-        const upWardStand = () => {
-            clearTimeout(this.timer);
-            this.timer = setTimeout(() => {
-                this.index++;
-                if (this.index >= this.wardStart) {
-                    this.index = this.standStart;
-                }
-
-                this.ctx.clearRect(
-                    canCenterX, canCenterY,
-                    eachWidth, eachHeight
-                );
-                this.ctx.drawImage(
-                    this.image,
-                    this.index * eachWidth, this.upCol * eachHeight,
-                    eachHeight, eachHeight,
-                    canCenterX, canCenterY,
-                    eachHeight, eachHeight
-                );
-                upWardStand();
-            }, this.standSpeed)
-        };
-        upWardStand();
+        stand()
     }
 };
 
@@ -286,8 +87,15 @@ const hero = new Character({
     imageSrc: "css_sprites.png",
     index: 4,
     imageCol: 12,
-    imageRow: 4
+    imageRow: 4,
+
+    wardSpeed: 100,
+    standSpeed: 300,
+
+    standStart: 0,
+    wardStart: 4,
+    wardEnd: 12
 })
 window.onload = () => {
-    hero.rightWardStand();
+    hero.stand(2);
 }
